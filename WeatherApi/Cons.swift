d78b0a507a680ad73a8cbccfb4b650433f4f086b
imran <imufun1@gmail.cm>
2017-11-06 09:07:07 -0800

    add

--- WeatherApi/Cons.swift
+++ WeatherApi/Cons.swift
@@ -8,5 +8,5 @@
 
 import Foundation
 let API = "http://api.openweathermap.org/data/2.5/weather?lat=23.7099&lon=90.4071&appid=f279df88f01232850ddff621c125603d"
-
+//let API = "http://api.openweathermap.org/data/2.5/weather?lat=13.01&lon=35.02&appid=f279df88f01232850ddff621c125603d"
 
--- WeatherApi/ViewController.swift
+++ WeatherApi/ViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 import Alamofire
 class ViewController: UIViewController {
     
+    
+    let url  = URL(string: API)!
     @IBOutlet weak var CityLabel: UILabel!
     @IBOutlet weak var stationLabel: UILabel!
     @IBOutlet weak var weatheTypeLabel: UILabel!
+    
+    @IBOutlet weak var weatherdes: UILabel!
     @IBOutlet weak var TempLabel: UILabel!
+    
+    @IBOutlet weak var WeatherImage: UIImageView!
     
     
     var city : String = ""
     var station : String = ""
     var weatherType : String = ""
     var temprature: Double = 0.0
-    
+    var descriptionText: String = ""
     //  var weather = Weatherity()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +40,7 @@ class ViewController: UIViewController {
     
     
     func getWeatherData(){
-        let url  = URL(string: API)!
+ 
         Alamofire.request(url).responseJSON { response in
             let result = response.result
             
@@ -43,14 +49,15 @@ class ViewController: UIViewController {
                 // City
                 if let name = dict["name"] as? String {
                     self.city = name
-                    self.CityLabel.text = self.city
+self.CityLabel.text = self.city
                     print(self.city)
                 }
                 // station
+              
                 if let station = dict["base"] as? String {
                     
                     self.station = station
-                    self.stationLabel.text = self.station
+                  //  self.stationLabel.text = self.station
                     print(self.station)
                 }
                 
@@ -60,15 +67,25 @@ class ViewController: UIViewController {
                         self.weatherType = main
                         self.weatheTypeLabel.text = self.weatherType
                     }
+                    if let des = weather[0]["description"] as? String {
+                        self.descriptionText = des
+                        self.weatherdes.text = self.descriptionText
+                    }
+                    if let img = weather[0]["icon"] as? UIImageView {
+                        self.WeatherImage = img
+                        
+                    }
+//                    if let description = dict["description"] as? [Dictionary<String, Any>]{
+//                        if let
+//                    }
                 }
                 
                 
                 if let main = dict["main"] as? Dictionary<String, AnyObject>{
                     if let temp = main["temp"] as? Double{
-                        self.temprature = temp
-                        self.TempLabel.text = "\(self.temprature)"
-                        
-                    }
+                        self.temprature = temp - 273.15
+                        self.TempLabel.text = String(format: "%.0f", self.temprature)
+                     }
                 }
                 
             }
@@ -77,7 +94,13 @@ class ViewController: UIViewController {
         }
     }
     func UpdateWeaher(){
-        // CityLabel.text = weather.city
+        
+       // let url  = URL(string: API)!
+        //CityLabel.text = weather.city
+//       Alamofire.request(url, method: .post, parameters: Parameters, encoding: JSONEncoding.default, headers: nil){
+//           response in
+//        }
+        
     }
 }
 
